08f8c538312db3f33d9aab7fd361605629dc12be
pear171902790 <171902790@qq.com>
2014-10-14 10:49:59 +0800

    kljljljljljljlkjljljljljlj

--- XYOL.MvcWeb/Controllers/HomeController.cs
+++ XYOL.MvcWeb/Controllers/HomeController.cs
@@ -3,17 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using XYOL.MvcWeb.ViewModel;
 
 namespace XYOL.MvcWeb.Controllers
 {
     public class HomeController : Controller
     {
+
+        public ActionResult Layout()
+        {
+            var vm = new HomeLayoutViewModel() {Title = "旬阳之窗"};
+            return PartialView(vm);
+        }
+
         //
         // GET: /Home/
-
         public ActionResult Index()
         {
-            return View();
+            var vm = new HomeIndexViewModel() {Test = "aaa"};
+            return View(vm);
+        }
+        
+        public ActionResult Part1()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Part2()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Part3()
+        {
+            return PartialView();
         }
 
     }
